141480289e37278681b1910146132bcf4ee2e784
kisangyun <syki97@naver.com>
2021-12-19 04:43:30 +0900

    edit arduino

--- arduino/waterpump.ino
+++ arduino/waterpump.ino
@@ -1,7 +1,9 @@
+// 워터 펌프 핀번호
 int AA = 5;
 int BB = 6;
 
-char command;
+//입력값 변수
+char command; 
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,13 +15,18 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Serial.available > 0) 
+  //입력값을 받았을 경우
+  
+  if(Serial.available() > 0) 
   {
+    command = Serial.read();
+    // 실행 입력값일 경우 워터펌프 킨다.
     if(command == 'a') 
     {
       digitalWrite(AA,HIGH);
       digitalWrite(BB,LOW);
     }
+    // 종료 입력값일 경우 워터펌프를 종료한다.
     else if(command == 's') 
     {
       digitalWrite(AA,LOW);
